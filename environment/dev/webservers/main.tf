b729a8472dd28a19fa2dd4e09434c360d46a5a6c
Arjoo Khattri <77634395+Arjoo-Khattri@users.noreply.github.com>
2025-02-02 15:45:55 -0500

    Update main.tf

--- environment/dev/webservers/main.tf
+++ environment/dev/webservers/main.tf
@@ -82,6 +82,14 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description      = "HTTP from everywhere for docker container port"
+    from_port        = 8081
+    to_port          = 8083
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
