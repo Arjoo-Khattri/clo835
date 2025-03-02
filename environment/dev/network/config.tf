0fcc0a0c19e78649402e4e36b5d2c7e6c350a1c4
Arjoo Khattri <77634395+Arjoo-Khattri@users.noreply.github.com>
2025-03-01 20:12:56 -0500

    Updated config.tf

--- environment/dev/network/config.tf
+++ environment/dev/network/config.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "clo835-assignment1-arjoo"      // Bucket where to SAVE Terraform State
+    bucket = "clo835-assignment2-arjoo"      // Bucket where to SAVE Terraform State
     key    = "dev/network/terraform.tfstate" // Object name in the bucket to SAVE Terraform State
     region = "us-east-1"                     // Region where bucket is created
   }
